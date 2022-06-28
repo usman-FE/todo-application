3517eb596a85dc54390f7d86830ee3cdbc7675a1
Usman-FE <ch.usman564@gmail.com>
2022-06-28 16:14:17 +0500

    Done dark/light toggle

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,5 +1,27 @@
-import { todoTasks } from "./todoData.js";
+import { todoTasks } from './todoData.js';
 
-const task = todoTasks[0]
+const toggle = document.querySelector('#toggle');
+const body = document.querySelector('body');
+const colorMode = localStorage.getItem('colorMode');
 
-console.log(task);
+toggle.addEventListener('click', function () {
+  body.classList.toggle('light');
+  body.classList.toggle('dark');
+  if (body.classList.contains('dark')) {
+    localStorage.setItem('colorMode', 'dark');
+  } else {
+    localStorage.setItem('colorMode', 'light');
+  }
+});
+
+window.addEventListener('load', function () {
+  if (!colorMode) return;
+  if (colorMode === 'dark') {
+    body.classList.add('dark');
+    body.classList.remove('light');
+  }
+  if (colorMode === 'light') {
+    body.classList.add('light');
+    body.classList.remove('dark');
+  }
+});
