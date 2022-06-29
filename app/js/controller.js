c9dfc9dc4a2a179348abc86f95df69795550e443
Muhammad Usman <ch.usman564@gmail.com>
2022-06-29 21:38:41 +0500

    Rendering task from the object

--- app/js/controller.js
+++ app/js/controller.js
@@ -3,6 +3,9 @@ import { todoTasks } from './todoData.js';
 const toggle = document.querySelector('#toggle');
 const body = document.querySelector('body');
 const colorMode = localStorage.getItem('colorMode');
+const taskContainer = document.querySelector('.task__list');
+
+// THEME TOGGLE
 
 toggle.addEventListener('click', function () {
   body.classList.toggle('light');
@@ -25,3 +28,34 @@ window.addEventListener('load', function () {
     body.classList.remove('dark');
   }
 });
+
+const tasks = todoTasks.todos;
+console.log(tasks);
+
+
+const generateMarkup = function (task) {
+  return `
+  <li class="task__item">
+  <div class="task__checkbox">
+  <span class="${task.isCompleted ? 'task__checked' : ''}"></span>
+  <a href="#" class="task__icon circle-icon">&nbsp;</a>
+  </div>
+  <p class="task__description ${task.isCompleted ? 'task-done' : ''}">${task.task}</p>
+  <div class="task__edit">
+  <a href="#" class="task__icon edit-icon controls">&nbsp;</a>
+  </div>
+  <div class="task__delete">
+  <a href="#" class="task__icon delete-icon controls">&nbsp;</a>
+  </div>
+  </li>
+  `
+}
+
+const markup = tasks.map(task => {
+  return generateMarkup(task);
+});
+
+
+markup.forEach(item => {
+  taskContainer.insertAdjacentHTML('beforeend', item)
+})
--- app/js/todoData.js
+++ app/js/todoData.js
@@ -1,17 +1,15 @@
-export const todoTasks = [
-  {
-    task: 'This is my first task',
-    isCompleted: false,
-    id: 1
-  },
-  {
-    task: 'This is my second task',
-    isCompleted: false,
-    id: 2
-  },
-  {
-    task: 'This is my third task',
-    isCompleted: false,
-    id: 3
-  }
-]
+export const todoTasks = {
+  todos: [
+    {
+      task: 'This is my first task',
+      isCompleted: false,
+      id: 1
+    },
+    {
+      task: 'This is my third task',
+      isCompleted: true,
+      id: 2
+    }
+  ],
+  id: 4,
+}
